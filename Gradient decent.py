605144e21b232a70a190d6f6ce95b408841378bf
Lujainy7 <154526545+Lujainy7@users.noreply.github.com>
2023-12-21 16:44:53 +0300

    Gradient decent

---
+++ Gradient decent.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Gradient Descent for  linear regression
+
+# In[65]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (7.0,5.0)
+
+# Preprocessing Input data
+data = pd.read_csv('insurance (1).csv')
+X = data.iloc[:, 0]
+Y = data.iloc[:, 1]
+plt.scatter(X, Y)
+plt.show()
+
+
+# In[68]:
+
+
+# Building the model
+m = 0
+c = 0
+
+L = 0.0001  # The learning Rate
+epochs = 1000  # The number of iterations to perform gradient descent
+
+n = float(len(X)) # Number of elements in X
+
+# Performing Gradient Descent 
+for i in range(epochs): 
+    Y_pred = m*X + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c
+    
+print (m, c)
+
+
+# In[73]:
+
+
+plt.scatter(X,Y)
+plt.scatter(X,Y_pred,color='orange')  
+plt.show()
+
+
+# In[74]:
+
+
+# Making predictions
+Y_pred = m*X + c
+
+plt.scatter(X, Y) 
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='orange')  # regression line
+plt.show()
+
